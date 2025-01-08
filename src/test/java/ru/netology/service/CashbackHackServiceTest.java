f9bcc87bed6621567c436f332541c340918dc26a
Anton Chukin <anton.chukin5@gmail.com>
2025-01-08 22:30:57 +0300

    JUnit5 introduced

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,10 @@
 package ru.netology.service;
 
 import org.junit.Test;
-
 import org.junit.Assert;
+
+//import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 //import org.testng.annotations.Test;
 
@@ -48,5 +50,45 @@ public class CashbackHackServiceTest {
         int expected = 0;
 
         Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateIfAmountIsLessThanBoundaryJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(600);
+        int expected = 400;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateIfAmountIsMoreThanBoundaryJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1400);
+        int expected = 600;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateIfAmountIsZeroJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(0);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateIfAmountEqualsBoundaryJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
     }
 }
